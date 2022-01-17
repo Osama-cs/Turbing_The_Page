2acb86c2ca0f5593e665758d86f1cbe39e705760
c1943625 <ilyaso@cardiff.ac.uk>
2022-01-17 21:41:57 +0000

    I have added all of the numbers to the page now.

--- lib/src/helplinespage.dart
+++ lib/src/helplinespage.dart
@@ -32,21 +32,35 @@ class _HelplinesPageState extends State<HelplinesPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Here are some mental health helplines that you can call if you ever feel down about yourself and need some extra help.",
-                style: TextStyle(fontSize: 20),
+                "Here are some mental health helplines that you can call if you ever feel down about yourself and need some extra help. (UK numebrs only).",
+                style: TextStyle(fontSize: 18),
               ),
               SizedBox(
                 height: 20,
               ),
               BulletedList(
-                listItems: ["", "hi"],
+                listItems: [
+                  "Samaritans. This is a more general mental health helpline that you can contact 24 hours, 365 days of a year. Number: 116 123.",
+                  "National Suicide Prevention Helpline UK. This is a helpline that offer support if anyone is dealing with suicidal thoughts, "
+                      "They are also open 24/7. Number: 0800 689 5652.",
+                  "The Mix. If you're under 25, you can call Mix to get some assistance, they are open between 3pm - 12am. Number:  0808 808 4994.",
+                  "Switchboard. This is for anyone that is apart of the LGBTQIA+ community, all phone operators identify as LGBTQIA+ as well. They are open between "
+                      "10am - 10pm. Number: 0300 330 0630.",
+                ],
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 18,
+                  fontSize: 16,
                 ),
                 bulletColor: Colors.black,
                 listOrder: ListOrder.ordered,
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Hopefully these numbers help you out in anyway <3",
+                style: TextStyle(fontSize: 16),
+              ),
             ],
           ),
         ),
